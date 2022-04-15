f7d2dbc29dedb3ea6063ea5b80caed3ca5c54490
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-04-15 15:03:25 +0900

    #1. 초기폴더링(재송부)

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Detail from "./pages/Detail";
+import Main from "./pages/Main";
+import PostWrite from "./pages/PostWrite";
+import MyPage from "../pages/MyPage";
 
 function App() {
   return (
@@ -7,9 +17,12 @@ function App() {
       <Grid>
         <Header></Header>
         <ConnectedRouter history={history}>
-          <Route path="/" exact component={PostList} />
+          <Route path="/" exact component={Main} />
           <Route path="/login" exact component={Login} />
           <Route path="/signup" exact component={Signup} />
+          <Route path="/PostWrite" exact component={PostWrite} />
+          <Route path="/MyPage" exact component={MyPage} />
+          <Route path="/Detail" exact component={Detail} />
         </ConnectedRouter>
       </Grid>
     </React.Fragment>
